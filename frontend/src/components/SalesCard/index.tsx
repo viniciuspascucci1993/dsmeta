57d2d2ca9c762d3cfc3b4a881d6105ffe3e3bc43
Vinicius Torres Pascucci[ <vinicius.pascucci1@gmail.com>
2022-09-10 15:44:00 -0300

    Date Update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,11 +17,15 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([])
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales/find-sales`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales/find-sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content)
             })
-    }, []);
+    }, [minDate, maxDate]);
     return (
         <>
             <div className="dsmeta-card">
