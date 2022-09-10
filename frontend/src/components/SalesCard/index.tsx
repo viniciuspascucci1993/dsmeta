939be58f5a9d9f717ca5c16ae9aba48ecb40a4c5
Vinicius Torres Pascucci[ <vinicius.pascucci1@gmail.com>
2022-09-10 15:52:34 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -72,7 +72,7 @@ function SalesCard() {
                                         <td>R$ {sale.amount.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}</td>
                                         <td>
                                             <div className="dsmeta-red-btn-container">
-                                                <NotificationButton />
+                                                <NotificationButton saleId={sale.id} />
                                             </div>
                                         </td>
                                     </tr>
